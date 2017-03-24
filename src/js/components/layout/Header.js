4aaebb6a8a86ea1637cf441b7c6a0de2aa46ab7a
Lasse Legaard <legaard.lasse@gmail.com>
2017-03-24 13:51:59 +0100

    Fixed navigation bug

--- src/js/components/layout/Header.js
+++ src/js/components/layout/Header.js
@@ -9,8 +9,12 @@ export default class Header extends React.Component {
     }
   }
 
-  showOrHideMenu() {
+  toggleMenu() {
     this.setState({show: !this.state.show});
+  }
+
+  hideMenu() {
+    this.setState({show: false});
   }
 
   render () {
@@ -18,15 +22,15 @@ export default class Header extends React.Component {
       <header>
         <div className="container">
           <img id="logo" src="/assets/img/logo.svg" alt="logo"/>
-          <i onClick={this.showOrHideMenu.bind(this)}
+          <i onClick={ (() => this.setState({show: !this.state.show})).bind(this) }
              className={"fa " + (this.state.show ? 'fa-times' : 'fa-bars')}
              aria-hidden="true"></i>
           <nav className={this.state.show ? 'show' : 'hide'}>
-            <Link onClick={this.showOrHideMenu.bind(this)}
+            <Link onClick={this.hideMenu.bind(this)}
                   to="/projects" activeClassName="active">Projects</Link>
-            <Link onClick={this.showOrHideMenu.bind(this)}
+            <Link onClick={ this.hideMenu.bind(this) }
                   to="/about" activeClassName="active">About</Link>
-            <Link onClick={this.showOrHideMenu.bind(this)}
+            <Link onClick={this.hideMenu.bind(this)}
                   to="/contact" activeClassName="active">Contact</Link>
           </nav>
         </div>
